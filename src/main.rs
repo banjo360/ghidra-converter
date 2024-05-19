ec973e2f41884dd332c751de879c24608620f9a0
minirop <minirop@hotmail.com>
2024-05-19 15:38:53 +0200

    generates header file containing function definitions (that don't have an 'undefined' parameter or return type)

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,29 @@ struct Args {
     #[arg()]
     input: String,
 
-    /// TXT file to write
+    /// filename to generate (without extension)
     #[arg()]
     output: String,
+}
+
+struct Function {
+    name: String,
+    return_type: String,
+    args: Vec<String>,
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    let mut output = File::options().create(true).write(true).truncate(true).open(args.output)?;
+    let mut output = File::options().create(true).write(true).truncate(true).open(format!("{}.txt", args.output))?;
+    let mut output_h = File::options().create(true).write(true).truncate(true).open(format!("{}.h", args.output))?;
+    let mut current_function = Function {
+        name: "".into(),
+        return_type: "".into(),
+        args: vec![],
+    };
 
+    let mut read_next = false;
     let file = File::open(args.input)?;
     let file = BufReader::new(file);
     let parser = EventReader::new(file);
@@ -41,32 +54,43 @@ fn main() -> std::io::Result<()> {
                     "symbol" => {
                         let mut address = 0;
                         let mut name = String::new();
+                        let mut source_type = String::new();
 
                         for attr in attributes {
                             let attr_name = attr.name.local_name.to_lowercase();
                             match attr_name.as_str() {
                                 "name" => name = attr.value,
                                 "address" => address = u32::from_str_radix(&attr.value, 16).unwrap(),
+                                "source_type" => source_type = attr.value,
                                 _ => {},
                             };
                         }
 
-                        writeln!(output, "{:08X} {}", address, name);
+                        if source_type == "USER_DEFINED" {
+                            writeln!(output, "{:08X} {}", address, name);
+                        }
                     },
+                    "typeinfo_cmt" => {
+                        read_next = true;
+                    }
                     "function" => {
                         let mut entry_point = 0;
                         let mut name = String::new();
+                        let mut namespace = String::new();
 
                         for attr in attributes {
                             let attr_name = attr.name.local_name.to_lowercase();
                             match attr_name.as_str() {
                                 "name" => name = attr.value,
                                 "entry_point" => entry_point = u32::from_str_radix(&attr.value, 16).unwrap(),
+                                "namespace" => namespace = attr.value,
                                 _ => {},
                             };
                         }
 
-                        writeln!(output, "{:08X} {}", entry_point, name);
+                        if namespace.len() == 0 {
+                            writeln!(output, "{:08X} {}", entry_point, name);
+                        }
                     },
                     _ => {},
                 };
@@ -74,6 +98,14 @@ fn main() -> std::io::Result<()> {
             Err(e) => {
                 eprintln!("Error: {e}");
                 break;
+            }
+            Ok(XmlEvent::Characters(text)) => {
+                if read_next {
+                    if !text.contains("undefined") {
+                        writeln!(output_h, "{}", text.replace(" __stdcall", ""));
+                    }
+                    read_next = false;
+                }
             }
             _ => {}
         }
